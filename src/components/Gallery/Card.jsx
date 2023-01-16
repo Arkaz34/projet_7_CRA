91cde3401def42439df3c86ec3e312b417cfb222
marion brothier <arnaudvv@hotmail.fr>
2023-01-16 11:21:41 +0100

    mise en place des cards

--- src/components/Gallery/Card.jsx
+++ src/components/Gallery/Card.jsx
@@ -2,8 +2,18 @@ import './card.css'
 
 export default function Card(){
     return(
-        <div>
-            <section>Card</section>
-        </div>
+            <section className='block-card'>
+                <div className='container-card-1'>
+                    <div className='card'>Card</div>
+                    <div className='card'>Card 2</div>
+                    <div className='card'>Card 3</div>
+                </div>
+                <div className='container-card-1'>
+                    <div className='card'>Card 4</div>
+                    <div className='card'>Card 5</div>
+                    <div className='card'>Card 6</div>
+
+                </div>
+            </section>
     )
 }
